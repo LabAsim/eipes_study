eb978936eabad55783f39e96f93a4ad2a052e107
LabAsim <lampgit@pm.me>
2024-07-09 12:54:24 +0300

    prompt for number of rows, refactor logic

--- main_send_reminders.py
+++ main_send_reminders.py
@@ -33,9 +33,27 @@ def main():
     # You may see this message if you email a total of more than 500 recipients in a single email
     # and or more than 500 emails sent in a day.
     excel_file_emails = compare_emails()
+
+    _input = input("How many rows do you need (warning: max 500emails/day)?")
+    try:
+        row_number = int(_input.strip())
+    except ValueError:
+        logger.error(f"{_input=} is not a integer!")
+        return None
+
+    _input = input(
+        "\nAre you sure that you checked that these emails are the ones that didn't answer and they are <=500?\n"
+    )
+    if _input not in ("yes", "Yes", "1", "y", "Y"):
+        logger.error("Double check the emails!")
+        return
+    # Slice the dataframe
+    excel_file_emails = excel_file_emails.iloc[:row_number]
+
     excel_file_emails = compare_save_emails_locally(
         df=excel_file_emails, excel_name="emails_sent_reminder.xlsx"
     )
+
     it = iterate_pandas_rows(df=excel_file_emails)
     send_reminders(creds=creds, it=it, subject=SUBJECT_REMINDER_EMAIL)
 
